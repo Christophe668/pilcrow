346b2aa1323f9d8464ec5739de64d59b355c4233
Christophe Pecsteen <christophe.pecsteen@gmail.com>
2026-05-06 16:43:14 +0200

    chore: bootstrap Expo + Expo Router project

---
+++ app.config.ts
@@ -0,0 +1,31 @@
+import type { ExpoConfig } from "@expo/config-types";
+
+const config: ExpoConfig = {
+  name: "wallabag",
+  slug: "wallabag",
+  scheme: "wallabag",
+  version: "0.1.0",
+  orientation: "portrait",
+  userInterfaceStyle: "automatic",
+  ios: {
+    bundleIdentifier: "com.cpecsteen.wallabag",
+    supportsTablet: true,
+  },
+  android: {
+    package: "com.cpecsteen.wallabag",
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+  },
+  plugins: [
+    "expo-router",
+    "expo-font",
+    "expo-secure-store",
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+};
+
+export default config;
